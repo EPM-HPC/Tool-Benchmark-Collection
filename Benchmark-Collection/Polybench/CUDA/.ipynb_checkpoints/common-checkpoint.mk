cab43f4345a3c9bca3b278e066c740bd20b29f9f
elio <2925539854@qq.com>
2022-06-16 21:27:31 +0800

    compile run Polybench

--- Benchmark-Collection/Polybench/CUDA/.ipynb_checkpoints/common-checkpoint.mk
+++ Benchmark-Collection/Polybench/CUDA/.ipynb_checkpoints/common-checkpoint.mk
@@ -6,6 +6,5 @@ GENCODE_SM61 ?= -gencode=arch=compute_61,code=sm_61
 
 all:
 	nvcc -O3 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_80,code=compute_80 ${NVCC_ADDITIONAL_ARGS} ${CUFILES} -o ${EXECUTABLE} 
-
 clean:
-	rm -f *~ *.out *.exe
+	rm -f *~ *.out
